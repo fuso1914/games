139e7c3a07f7b1f799b9a7623d98c5da70b39a25
fuso <47074438+fuso1914@users.noreply.github.com>
2019-05-04 23:17:28 +0900

    mob set

---
+++ Godot/tutorial/Mob.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+var mob_types = ["walk", "swim", "fly"]
+
+func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
