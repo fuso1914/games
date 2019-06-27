4bdcb858ea429156db428749206f1b263cddc278
fuso1914 <fuso1914@gmail.com>
2019-06-27 15:50:29 +0900

    mac

---
+++ Mobile game/build/default/main/main_generated_2.go
@@ -0,0 +1,30 @@
+components {
+  id: "damage"
+  component: "/main/factory/damage.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "damage_factory"
+  component: "/main/scripts/damage_factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
